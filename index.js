dbf217d078c6f41e94dcb0aa1c50112bdbd5647c
Violet58 <larasoraya276@gmail.com>
2026-04-26 03:35:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Kyra está viva!');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor web ativo');
+});
+
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
